34260291dfca6091a97a4c16d948291b529ba038
arxaqapi <arxaqapi@protonmail.com>
2022-12-04 15:47:41 +0100

    feat: day04.2

--- 2022/solutions/lib/day04.ml
+++ 2022/solutions/lib/day04.ml
@@ -1,20 +1,15 @@
 let filename = "data/04_input"
 
-(*
-each line, pair of sections ids   
-*)
-
-(*
-2.4 -> 234
-3.3 -> 3   
-*)
-
-let is_contained l =
+let is_contained (l : int list) =
   match l with
   | [ a; b; c; d ] -> (a <= c && d <= b) || (c <= a && b <= d)
   | _ -> failwith "what?"
 
-let get_lines_n_sum f =
+(* https://github.com/hellopatrick/xmas/blob/v2022/day04/main.ml *)
+let do_overlap (l : int list) =
+  match l with [ a; b; c; d ] -> a <= d && b >= c | _ -> failwith "what?"
+
+let get_lines_n_sum f cmpf =
   let chan = open_in f in
   let rec loop s =
     match In_channel.input_line chan with
@@ -23,7 +18,7 @@ let get_lines_n_sum f =
         match
           String.split_on_char ',' l
           |> List.map (String.split_on_char '-')
-          |> List.flatten |> List.map int_of_string |> is_contained
+          |> List.flatten |> List.map int_of_string |> cmpf
         with
         | true -> loop (succ s)
         | false -> loop s)
@@ -32,4 +27,6 @@ let get_lines_n_sum f =
   close_in chan;
   res
 
-let run () = Printf.printf "[04] - Part 1: %d\n" @@ get_lines_n_sum filename
+let run () =
+  Printf.printf "[04] - Part 1: %d\n" @@ get_lines_n_sum filename is_contained;
+  Printf.printf "[04] - Part 2: %d\n" @@ get_lines_n_sum filename do_overlap
