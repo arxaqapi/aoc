5f537f6700a0d1569714f96baa6b6e5affa53ca8
arxaqapi <arxaqapi@protonmail.com>
2022-12-08 10:05:01 +0100

    feat: base day 7

---
+++ 2022/solutions/lib/day07.ml
@@ -0,0 +1,14 @@
+let filename = "data/07_input"
+
+
+(*
+sum of dir where size is bigger than => 10_000   
+count nested files also
+
+$ ls => list of files:
+  328909 dwvphbm = size name
+  dir akjeliub = type name
+$ cd .. => go back
+$ cd azyge => cd into dir
+*)
+
