57ce362cb27130a0777545d03cff21f96866642c
arxaqapi <arxaqapi@protonmail.com>
2022-12-22 16:07:59 +0100

    backup

--- 2022/solutions/lib/day08.ml
+++ 2022/solutions/lib/day08.ml
@@ -21,56 +21,54 @@ module Forest = struct
 
   let get_tree f i j = Hashtbl.find_opt f.forest (i, j)
 
-  let add_tree f i j h = match get_tree f i j with
-  (* deduplication *)
-  | Some _ -> failwith "Already exists"
-  | None -> Hashtbl.add f.forest (i, j) (Tree.create h)
-
-  (* only check for a straight line, no recursion *)
-  let solve_visibility f i j = 
-    let _directions = [ (-1, 0); (1, 0); (0, -1); (0, 1) ] in
-    
-    (* let rec solver i j = match get_tree f i j with *)
-      (* | None -> failwith "No tree found" *)
-      (* | Some x -> if (match x.is_visible with None -> false | Some t -> t) then  *)
-  ()
-
-
-
-  (* let is_visible f i j =
-    (* Check if not out of bounds (<0, <0) *)
-    if i < 0 || j < 0 || i >= f.height || j >= f.width then true
-    else Hashtbl.find f.forest (i, j) |> Tree.is_visible
-
   let add_tree f i j h =
-    match Hashtbl.find_opt f (i, j) with
+    match get_tree f i j with
     (* deduplication *)
     | Some _ -> failwith "Already exists"
-    | None -> Hashtbl.add f (i, j) (Tree.create h)
+    | None -> Hashtbl.add f.forest (i, j) (Tree.create h)
 
-  let get_tree f i j =
-    match Hashtbl.find_opt f.forest (i, j) with None -> failwith "inexistant"
+  (* only check for a straight line, no recursion *)
+  let solve_visibility _f _i _j =
+    let _directions = [ (-1, 0); (1, 0); (0, -1); (0, 1) ] in
 
-  let tree_is_visible f i j =
-    let neighbour_offset = [ (-1, 0); (1, 0); (0, -1); (0, 1) ] in
-    let rec search_visibility offs =
-      match offs with
-      | [] -> false
-      | (i_off, j_off) :: tl ->
-          let current_height = 0 in
-          (* wtf *)
-          let neighbour = get_tree f (i + i_off) (j + j_off) in
-          if
-            is_visible f (i + i_off) (j + j_off)
-            && neighbour.height < current_height
-          then true
-          else search_visibility tl
-    in
-    search_visibility neighbour_offset *)
+    (* let rec solver i j = match get_tree f i j with *)
+    (* | None -> failwith "No tree found" *)
+    (* | Some x -> if (match x.is_visible with None -> false | Some t -> t) then  *)
+    ()
+
+  (* let is_visible f i j =
+       (* Check if not out of bounds (<0, <0) *)
+       if i < 0 || j < 0 || i >= f.height || j >= f.width then true
+       else Hashtbl.find f.forest (i, j) |> Tree.is_visible
+
+     let add_tree f i j h =
+       match Hashtbl.find_opt f (i, j) with
+       (* deduplication *)
+       | Some _ -> failwith "Already exists"
+       | None -> Hashtbl.add f (i, j) (Tree.create h)
+
+     let get_tree f i j =
+       match Hashtbl.find_opt f.forest (i, j) with None -> failwith "inexistant"
+
+     let tree_is_visible f i j =
+       let neighbour_offset = [ (-1, 0); (1, 0); (0, -1); (0, 1) ] in
+       let rec search_visibility offs =
+         match offs with
+         | [] -> false
+         | (i_off, j_off) :: tl ->
+             let current_height = 0 in
+             (* wtf *)
+             let neighbour = get_tree f (i + i_off) (j + j_off) in
+             if
+               is_visible f (i + i_off) (j + j_off)
+               && neighbour.height < current_height
+             then true
+             else search_visibility tl
+       in
+       search_visibility neighbour_offset *)
 end
 
 (* return true if is visible, recursive check base on struct (use ) *)
-
 
 (*
 forest.add_tree height: ! None visibility
--- 2022/solutions/lib/day10.ml
+++ 2022/solutions/lib/day10.ml
@@ -21,11 +21,22 @@ get line
 module Register = struct
   type t = { mutable x : int; mutable total : int; mutable current_cycle : int }
 
+  let do_overlap r = 
+    let cc = r.current_cycle mod 40 in
+    (cc >= pred r.x) && (cc <= succ r.x)
+
+  (** Make a cycle step *)
   let step r =
     (match r.current_cycle with
     | 20 | 60 | 100 | 140 | 180 | 220 ->
         r.total <- r.total + (r.current_cycle * r.x)
     | _ -> ());
+    r.current_cycle <- r.current_cycle + 1
+
+  let step_v2 r =
+    let s = if do_overlap r then "#" else "." in
+    Printf.printf "%s" s;
+    if r.current_cycle mod 40 = 0 then print_newline ();
     r.current_cycle <- r.current_cycle + 1
 
   let add r v = r.x <- r.x + v
@@ -41,17 +52,28 @@ let parse_line r s =
           Register.step r;
           Register.add r value)
 
-let parse_file f =
+let parse_line_v2 r s =
+  match String.length s with
+  | 4 -> Register.step_v2 r (* noop, cycle += 1*)
+  | _ ->
+      Scanf.sscanf s "addx %d" (fun value ->
+          Register.step_v2 r;
+          Register.step_v2 r;
+          Register.add r value)
+
+let parse_file f parse_line_v =
   let chan = open_in f in
   let register = Register.create () in
   let rec loop () =
     match In_channel.input_line chan with
     | None -> ()
     | Some l ->
-        parse_line register l;
+        parse_line_v register l;
         loop ()
   in
   loop ();
   register.total
 
-let run () = Printf.printf "[10] - %d\n" @@ parse_file filename
+let run () =
+  Printf.printf "[10] - Part 1: %d\n" @@ parse_file filename parse_line;
+  Printf.printf "[10] - Part 2: %d\n" @@ parse_file filename parse_line_v2
