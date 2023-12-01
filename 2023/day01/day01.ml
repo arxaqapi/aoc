598a60275c11c12a2d173e05781295d13b413b32
arxaqapi <arxaqapi@protonmail.com>
2023-12-01 19:43:20 +0100

    2023: day01 part2 ocaml

--- 2023/day01/day01.ml
+++ 2023/day01/day01.ml
@@ -1,51 +1,41 @@
-
 let input_data = "input_01.txt"
-(* 
-- each line is a calibration value to retrieve
-- the value is the first encountered digit concatenated to the last digit
+(*
+   - each line is a calibration value to retrieve
+   - the value is the first encountered digit concatenated to the last digit
 *)
 
-let is_number c = 
-  let ascii_of_c = Char.code c in 
+let is_number c =
+  let ascii_of_c = Char.code c in
   ascii_of_c >= 48 && ascii_of_c <= 57
 
 let get_number_from_char c = int_of_char c - int_of_char '0'
 
-  (* !! Can overflow! no safeguards *)
-let rec search s i incr = match s.[i] with
+(* !! Can overflow! no safeguards *)
+let rec search s i incr =
+  match s.[i] with
   | c when is_number c -> get_number_from_char c
   | c -> search s (i + incr) incr
 
 let retrieve_calibration_value s =
-  search s 0 1, search s (String.length s - 1) (- 1)
-
+  (search s 0 1, search s (String.length s - 1) (-1))
 
 let part_1_content f =
   (* while not none, read input and retrieve calibration value *)
-  let rec loop_input chan total = match In_channel.input_line chan with
+  let rec loop_input chan total =
+    match In_channel.input_line chan with
     | None -> total
-    | Some s -> 
-      let values = retrieve_calibration_value s in
-      Printf.printf "values: %d %d\n" (fst values) (snd values) ;
-      loop_input chan (total + (fst values * 10 + snd values))
-  in loop_input (open_in f) 0
-
+    | Some s ->
+        let values = retrieve_calibration_value s in
+        loop_input chan (total + ((fst values * 10) + snd values))
+  in
+  loop_input (open_in f) 0
 
 (* Part 2 *)
-let patterns = [|
-  "one"
-  ;"two"
-  ;"three"
-  ;"four"
-  ;"five"
-  ;"six"
-  ;"seven"
-  ;"eight"
-  ;"nine"
-  |]
+let patterns =
+  [ "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine" ]
 
 (* overlap is not taken into account *)
-let string_to_int s = 
+let string_to_int s =
   match s with
   | "one" -> 1
   | "two" -> 2
@@ -58,27 +48,48 @@ let string_to_int s =
   | "nine" -> 9
   | _ -> failwith "not a decimal"
 
-let check_pattern s i pattern = 
+let check_pattern s i pattern =
   (* check that pattern is shorter or equal to string lenght and current position *)
-  match pattern == String.sub s i (String.length pattern) with
-  | true -> Some (string_to_int pattern)
+  match String.length pattern + i <= String.length s with
   | false -> None
+  | true -> (
+      match pattern = String.sub s i (String.length pattern) with
+      | true -> Some (string_to_int pattern)
+      | false -> None)
 
-let check_all_patterns s i = check_pattern s i patterns.(0)
+let check_all_patterns s i =
+  let rec loop pattern =
+    match pattern with
+    | [] -> None
+    | hd :: tl -> (
+        match check_pattern s i hd with None -> loop tl | Some e -> Some e)
+  in
+  loop patterns
 
-(* for each i in string: 
-    if decimal, add to queue and i + 1
-    else if is pattern, get int from string and move i + 2
-*)
-(* let aaaaaaa s = 
-  let q = Queue.create () in
-  String.iteri (fun i c -> match is_number c with
-    | true -> Queue.push (get_number_from_char c) q
-    | false -> let _ = check_all_patterns s i in ()
-  ) s;
-  Queue.peek + Queue. *)
+let string_to_calibration_v s =
+  let i_list =
+    String.to_seq s |> List.of_seq
+    |> List.fold_left2
+        (fun b i c ->
+          match is_number c with
+          | true -> get_number_from_char c :: b
+          | false -> (
+              match check_all_patterns s i with None -> b | Some x -> x :: b))
+        []
+        (List.init (String.length s) (fun i -> i))
+  in
+  (10 * (i_list |> List.rev |> List.hd)) + List.hd i_list
 
+let part_2 f =
+  let rec loop_input chan total =
+    match In_channel.input_line chan with
+    | None -> total
+    | Some s -> loop_input chan (total + string_to_calibration_v s)
+  in
+  loop_input (open_in f) 0
 
-let () = 
+let () =
   (* 55386 , 54824 *)
-  Printf.printf "[01] part 1: %d | part 2: %d\n" (part_1_content input_data) 0 
+  Printf.printf "[01] part 1: %d | part 2: %d\n"
+    (part_1_content input_data)
+    (part_2 input_data)
