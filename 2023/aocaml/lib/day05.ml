9225f9ec83b406219283309e301f955343ad2d24
arxaqapi <arxaqapi@protonmail.com>
2023-12-06 14:30:08 +0100

    day05: found and solved the error

--- 2023/aocaml/lib/day05.ml
+++ 2023/aocaml/lib/day05.ml
@@ -46,6 +46,7 @@ let find_dest_in_map (mappings : map) input =
   in
   loop mappings
 
+(* Takes [all_mappings] and reduces it to the location value from the initial [seed].  *)
 let seed_to_location all_mappings seed =
   List.fold_left (fun base e -> find_dest_in_map e base) seed all_mappings
 
@@ -54,9 +55,64 @@ let solve_part_1 input =
   let all_mappings, seeds = parse input in
   List.map (seed_to_location all_mappings) seeds
   |> List.fold_left min Int.max_int
+
+(* Applies [f_map] on each value in the range of [start] + [len] and folds/reduces with [f]. *)
+let fold_left_map_range f accu f_map (start, len) =
+  let rec loop acc i =
+    match i with
+    | i when i < len -> loop (f acc (f_map (start + i))) (succ i)
+    | _ -> acc
+  in
+  loop accu 0
+
+let reduce_map_range all_mappings (start, len) =
+  let min_loc = ref Int.max_int in
+  for i = 0 to len - 1 do
+    (* start + i *)
+    min_loc := min !min_loc (seed_to_location all_mappings (start + i))
+  done;
+  !min_loc
+
+(* let seed_range_list (start, length) = List.init length (fun i -> start + i) *)
+
+let solve_part_2 input =
+  let all_mappings, _seeds = parse input in
+  let test =
+    [
+      (3136945476, 509728956);
+      (1904897211, 495273540);
+      (1186343315, 66026055);
+      (1381149926, 11379441);
+      (4060485949, 190301545);
+      (444541979, 351779229);
+      (1076140984, 104902451);
+      (264807001, 60556152);
+      (3676523418, 44140882);
+      (3895155702, 111080695);
+    ]
+  in
+
+  (* Iterative solution w. stack explosion *)
+  (* List.fold_left
+     (fun acc e ->
+       min acc
+         (List.map (seed_to_location all_mappings) (seed_range_list e)
+         |> List.fold_left min Int.max_int))
+     Int.max_int test *)
+
+  (* Recursive solution w. handcrafted fold *)
+  List.map
+    (fold_left_map_range min Int.max_int (seed_to_location all_mappings))
+    test
+  |> List.fold_left min Int.max_int
+
+(* for loop solution with index *)
+(* List.map (reduce_map_range all_mappings) test
+   |> List.fold_left min Int.max_int *)
+
 let solve () =
   let input = Io.load_as_list "data/05_input.txt" in
   (* 111627841 *)
   Printf.printf "[05] - Part 1: %d\n" @@ solve_part_1 input;
-  (* 69323689 & 475615938 are too high *)
-  Printf.printf "[05] - Part 2: %d\n" @@ solve_part_2 input
+  (* 69323688 *)
+  Printf.printf "[05] - Part 2: %d\n\n" @@ solve_part_2 input
