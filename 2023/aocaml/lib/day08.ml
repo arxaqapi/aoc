3400107c4d4c588e3320682ef9371b7109f2fcad
arxaqapi <arxaqapi@protonmail.com>
2023-12-10 02:05:07 +0100

    day08: part 2 bruteforce not working

--- 2023/aocaml/lib/day08.ml
+++ 2023/aocaml/lib/day08.ml
@@ -2,34 +2,67 @@ let parse_line line =
   let key = String.sub line 0 3 in
   let left = String.sub line 7 3 in
   let right = String.sub line 12 3 in
-  key, left, right
-
+  (key, left, right)
 
 let parse input = 
   let instruction_list = List.hd input in
   let table = Hashtbl.create 700 in
-  List.iter (fun line ->  
+  List.iter
+    (fun line ->
     let key, left, right = parse_line line in
-    Hashtbl.add table key (left, right) ) (List.tl @@ List.tl input);
-    instruction_list, table
+      Hashtbl.add table key (left, right))
+    (List.tl @@ List.tl input);
+  (instruction_list, table)
 
 let infinite_loop instructions table = 
-  let rec loop seq current_key = match seq () with
+  let rec loop seq current_key =
+    match seq () with
     | Seq.Nil -> failwith "Empty Sequence"
-    | Seq.Cons (x, xs) ->     
+    | Seq.Cons (x, xs) -> (
     match Hashtbl.find table current_key with
       | _ when current_key = "ZZZ" -> 0
-      | left_key, right_key -> 1 + match x with
-        | 'L' -> loop xs left_key
-        | 'R' -> loop xs right_key
-    in loop (Seq.cycle instructions) "AAA"
+        | left_key, right_key -> (
+            1
+            + match x with 'L' -> loop xs left_key | 'R' -> loop xs right_key))
+  in
+  loop (Seq.cycle instructions) "AAA"
 
+let end_with_a_keys table =
+  Hashtbl.fold
+    (fun key _ acc ->
+      if String.ends_with ~suffix:"A" key then key :: acc else acc)
+    table []
+let infinite_loop_p2 instructions table =
+  let rec loop seq current_keys =
+    match seq () with
+    | Seq.Nil -> failwith "Empty Sequence"
+    | Seq.Cons (x, xs) -> (
+        (* Return list of next keys using l or r *)
+        let new_keys =
+          List.map
+            (fun current_key ->
+              match Hashtbl.find table current_key with
+              | left_key, right_key -> (
+                  match x with 'L' -> left_key | 'R' -> right_key))
+            current_keys
+        in
+        match
+          List.fold_left
+            (fun acc e -> acc && String.ends_with ~suffix:"Z" e)
+            true new_keys
+        with
+        | true -> 0
+        | false -> 1 + loop xs new_keys)
+  in
+  loop (Seq.cycle instructions) (end_with_a_keys table)
 
 let solve_part_1 input = 
   let instruction_list, table = parse input in
   infinite_loop (String.to_seq instruction_list) table
 
-let solve_part_2 input = List.length input
+let solve_part_2 input =
+  let instruction_list, table = parse input in
+  infinite_loop_p2 (String.to_seq instruction_list) table
 
 let solve () =
   let input = Io.load_as_list "data/08_input.txt" in
